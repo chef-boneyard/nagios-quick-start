dbe054e3d325e62169301c97f8ca4ba4ca2b4c27
jtimberman <joshua@opscode.com>
2012-03-30 16:02:19 -0700

    Import nagios version 1.2.4

--- cookbooks/nagios/recipes/server.rb
+++ cookbooks/nagios/recipes/server.rb
@@ -2,11 +2,12 @@
 # Author:: Joshua Sierles <joshua@37signals.com>
 # Author:: Joshua Timberman <joshua@opscode.com>
 # Author:: Nathan Haneysmith <nathan@opscode.com>
+# Author:: Seth Chisamore <schisamo@opscode.com>
 # Cookbook Name:: nagios
 # Recipe:: server
 #
 # Copyright 2009, 37signals
-# Copyright 2009-2010, Opscode, Inc
+# Copyright 2009-2011, Opscode, Inc
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -26,7 +27,19 @@ include_recipe "apache2::mod_rewrite"
 include_recipe "nagios::client"
 
 sysadmins = search(:users, 'groups:sysadmin')
-nodes = search(:node, "hostname:[* TO *] AND app_environment:#{node[:app_environment]}")
+
+nodes = search(:node, "hostname:[* TO *] AND chef_environment:#{node.chef_environment}")
+
+begin
+  services = search(:nagios_services, '*:*')
+rescue Net::HTTPServerException
+  Chef::Log.info("Search for nagios_services data bag failed, so we'll just move on.")
+end
+
+if services.nil? || services.empty?
+  Chef::Log.info("No services returned from data bag search.")
+  services = Array.new
+end
 
 if nodes.empty?
   Chef::Log.info("No nodes returned from search, using this node so hosts.cfg has data")
@@ -43,65 +56,58 @@ role_list = Array.new
 service_hosts= Hash.new
 search(:role, "*:*") do |r|
   role_list << r.name
-  search(:node, "role:#{r.name} AND app_environment:#{node[:app_environment]}") do |n|
+  search(:node, "role:#{r.name} AND chef_environment:#{node.chef_environment}") do |n|
     service_hosts[r.name] = n['hostname']
   end
 end
 
-if node[:public_domain]
-  public_domain = node[:public_domain]
+if node['public_domain']
+  public_domain = node['public_domain']
 else
-  public_domain = node[:domain]
+  public_domain = node['domain']
 end
 
-%w{ nagios3 nagios-nrpe-plugin nagios-images }.each do |pkg|
-  package pkg
-end
-
-service "nagios3" do
-  supports :status => true, :restart => true, :reload => true
-  action [ :enable ]
-end
+include_recipe "nagios::server_#{node['nagios']['server']['install_method']}"
 
 nagios_conf "nagios" do
   config_subdir false
 end
 
-directory "#{node[:nagios][:dir]}/dist" do
-  owner "nagios"
-  group "nagios"
+directory "#{node['nagios']['conf_dir']}/dist" do
+  owner node['nagios']['user']
+  group node['nagios']['group']
   mode "0755"
 end
 
-directory node[:nagios][:state_dir] do
-  owner "nagios"
-  group "nagios"
+directory node['nagios']['state_dir'] do
+  owner node['nagios']['user']
+  group node['nagios']['group']
   mode "0751"
 end
 
-directory "#{node[:nagios][:state_dir]}/rw" do
-  owner "nagios"
-  group node[:apache][:user]
+directory "#{node['nagios']['state_dir']}/rw" do
+  owner node['nagios']['user']
+  group node['apache']['user']
   mode "2710"
 end
 
-execute "archive default nagios object definitions" do
-  command "mv #{node[:nagios][:dir]}/conf.d/*_nagios*.cfg #{node[:nagios][:dir]}/dist"
-  not_if { Dir.glob(node[:nagios][:dir] + "/conf.d/*_nagios*.cfg").empty? }
+execute "archive-default-nagios-object-definitions" do
+  command "mv #{node['nagios']['config_dir']}/*_nagios*.cfg #{node['nagios']['conf_dir']}/dist"
+  not_if { Dir.glob("#{node['nagios']['config_dir']}/*_nagios*.cfg").empty? }
 end
 
-file "#{node[:apache][:dir]}/conf.d/nagios3.conf" do
+file "#{node['apache']['dir']}/conf.d/nagios3.conf" do
   action :delete
 end
 
-case node[:nagios][:server_auth_method]
+case node['nagios']['server_auth_method']
 when "openid"
   include_recipe "apache2::mod_auth_openid"
 else
-  template "#{node[:nagios][:dir]}/htpasswd.users" do
+  template "#{node['nagios']['conf_dir']}/htpasswd.users" do
     source "htpasswd.users.erb"
-    owner "nagios"
-    group node[:apache][:user]
+    owner node['nagios']['user']
+    group node['apache']['user']
     mode 0640
     variables(
       :sysadmins => sysadmins
@@ -113,12 +119,29 @@ apache_site "000-default" do
   enable false
 end
 
-template "#{node[:apache][:dir]}/sites-available/nagios3.conf" do
+directory "#{node['nagios']['conf_dir']}/certificates" do
+  owner node['apache']['user']
+  group node['apache']['user']
+  mode "700"
+end
+
+bash "Create SSL Certificates" do
+  cwd "#{node['nagios']['conf_dir']}/certificates"
+  code <<-EOH
+  umask 077
+  openssl genrsa 2048 > nagios-server.key
+  openssl req -subj "#{node['nagios']['ssl_req']}" -new -x509 -nodes -sha1 -days 3650 -key nagios-server.key > nagios-server.crt
+  cat nagios-server.key nagios-server.crt > nagios-server.pem
+  EOH
+  not_if { ::File.exists?("#{node['nagios']['conf_dir']}/certificates/nagios-server.pem") }
+end
+
+template "#{node['apache']['dir']}/sites-available/nagios3.conf" do
   source "apache2.conf.erb"
   mode 0644
   variables :public_domain => public_domain
-  if File.symlink?("#{node[:apache][:dir]}/sites-enabled/nagios3.conf")
-    notifies :reload, resources(:service => "apache2")
+  if ::File.symlink?("#{node['apache']['dir']}/sites-enabled/nagios3.conf")
+    notifies :reload, "service[apache2]"
   end
 end
 
@@ -130,12 +153,19 @@ apache_site "nagios3.conf"
   end
 end
 
-%w{ commands templates timeperiods}.each do |conf|
+%w{ templates timeperiods}.each do |conf|
   nagios_conf conf
 end
 
+nagios_conf "commands" do
+  variables :services => services
+end
+
 nagios_conf "services" do
-  variables :service_hosts => service_hosts
+  variables(
+    :service_hosts => service_hosts,
+    :services => services
+  )
 end
 
 nagios_conf "contacts" do
@@ -148,4 +178,10 @@ end
 
 nagios_conf "hosts" do
   variables :nodes => nodes
+end
+
+service "nagios" do
+  service_name node['nagios']['server']['service_name']
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start ]
 end
--- cookbooks/nagios/resources/nrpecheck.rb
+++ cookbooks/nagios/resources/nrpecheck.rb
@@ -1,9 +1,9 @@
 #
-# Author:: Joshua Sierles <joshua@37signals.com>
+# Author:: Jake Vanderdray <jvanderdray@customink.com>
 # Cookbook Name:: nagios
-# Library:: default
+# Resource:: nrpecheck
 #
-# Copyright 2009, 37signals
+# Copyright 2011, CustomInk LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,19 +16,18 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-def nagios_boolean(true_or_false)
-  true_or_false ? "1" : "0"
-end
 
-def nagios_interval(seconds)
-  if seconds.to_i < node['nagios']['interval_length'].to_i
-    raise ArgumentError, "Specified nagios interval of #{seconds} seconds must be equal to or greater than the default interval length of #{node['nagios']['interval_length']}"
-  end
-  interval = seconds / node['nagios']['interval_length']
-  interval
-end
+actions :add, :remove
 
-def nagios_attr(name)
-  node['nagios'][name]
+# Name of the nrpe check, used for the filename and the command name
+attribute :command_name, :kind_of => String, :name_attribute => true
+
+attribute :warning_condition, :kind_of => String, :default => nil
+attribute :critical_condition, :kind_of => String, :default => nil
+attribute :command, :kind_of => String
+attribute :parameters, :kind_of => String, :default => nil
+
+def initialize(*args)
+  super
+  @action = :add
 end
